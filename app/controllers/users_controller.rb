92ce0627f00be420c4d71683f5c97a7e39c01d1a
Brian <b.perez3237@gmail.com>
2022-08-18 17:39:59 -0400

    Add user controller functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
-    validates :name, presence: true, uniqueness: true
+    skip_before_action :authorized, only: [:create, :show]
+
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        user = User.find(params[:id])
+        if user
+            render json: user
+        else
+            render json: { error: 'User not found'}, status: :unauthorized
+        end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :cost_codes
+
+    has_secure_password
+
+    validates :name, presence: true, uniqueness: true
 end
