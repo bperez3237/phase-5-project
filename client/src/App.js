7104a585788405f26499fd49f2f6dad85888420c
Brian <b.perez3237@gmail.com>
2022-08-18 19:22:45 -0400

    Fix app component bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,22 @@
-import {useEffect, useState} from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ActivityList from './containers/ActivityList';
-import CostCodesList from './containers/CostCodesList'
+import {useEffect, useState} from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigator from './containers/Navigator';
 import LoginPage from './pages/LoginPage';
 import ReviewActivities from './pages/ReviewActivities';
-import Upload from './pages/Upload'
+import Upload from './pages/Upload';
 import ViewCodes from './pages/ViewCodes';
 
 
 function App() {
-  
-
-
   return (
     <BrowserRouter>
     <Navigator></Navigator>
-      <Routes>
+      <Switch>
         <Route path='/' element={<ViewCodes />} />
         <Route path='/upload' element={<Upload />} />
         <Route path='/review_activities' element={<ReviewActivities />} />
         <Route path='/login' element={<LoginPage />} />
-
-      </Routes>
+      </Switch>
     </BrowserRouter>
   );
 }
