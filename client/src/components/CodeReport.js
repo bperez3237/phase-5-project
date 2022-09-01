7dab930e652b53b62fc260f80f1da54aeb841e4e
Brian <b.perez3237@gmail.com>
2022-08-31 21:06:44 -0400

    Update report layout

--- client/src/components/CodeReport.js
+++ client/src/components/CodeReport.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
 
 function CodeReport({costCode}) {
 
@@ -7,23 +8,27 @@ function CodeReport({costCode}) {
         console.log(e.target)
         console.log(e.target.value)
     }
-    const activityElems = costCode.activities.map((activity)=>{
-
-        // console.log(activity)
-        return(
-        <p key={activity.id}>{activity.description} - {activity.total_hours}</p>)
-    })
+    const activityElems = costCode.activities.map((activity)=><p key={activity.id}>{activity.description} - {activity.total_hours}</p>)
     // doesnt get total hours correctly because it gets activity from cost report route. This param is creted when Activity serializer is run
 
     return(
-        <div style={{border:'1px', borderStyle:'solid'}} onClick={handleClick} value={costCode.id}>
+        <Container style={{border:'1px', borderStyle:'solid'}}>
+            <Row>
             <h1>{costCode.code} - {costCode.name}</h1>
-            <h3>Last Week Hours: {costCode.current_hours}</h3>
-            <h3>Last Week Quantity: {costCode.current_quantity}</h3>
-            <h3>Production Rate: {Math.round(100*(costCode.current_quantity/costCode.current_hours))/100}</h3>
-            <h3 onClick={handleClick} value={costCode.id}>Activities:</h3>
+            <Col>
+            <p>Last Week Hours: {costCode.current_hours}</p>
+            <p>Last Week Quantity: {costCode.current_quantity}</p>
+            <p>Last Week Production Rate: {Math.round(100*(costCode.current_quantity/costCode.current_hours))/100}</p>
+            </Col>
+            <Col>
+            <p>Budget Hours: {costCode.current_hours}</p>
+            <p>Budget Quantity: {costCode.current_quantity}</p>
+            <p>Budget Production Rate: {Math.round(100*(costCode.current_quantity/costCode.current_hours))/100}</p>
+            </Col>
+            <p >Activities:</p>
             {activityElems}
-        </div>
+            </Row>
+        </Container>
     )
 }
 
