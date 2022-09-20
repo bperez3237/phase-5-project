3038c92ed961a46503a77f1771b9042dee5d3b9e
Brian <b.perez3237@gmail.com>
2022-09-20 19:21:27 -0400

    Remove activity status

--- client/src/components/Upload/ActivityStatus.js
+++ client/src/components/Upload/ActivityStatus.js
@@ -1,15 +1,10 @@
 import { Container } from "react-bootstrap";
-import useFetch from '../../hooks/useFetch'
-import { formatNumber, formatCurrency } from '../../services/Format/FormatNumber'
 
 
 function ActivitiesStatus() {
-    const {data} = useFetch('/activity_status')
 
     return(
         <Container className='m-3' style={{border:'1px', borderStyle:'solid'}}>
-            <h2>Work Week Hours: {formatNumber(data?.all_hours)}</h2>
-            <h2>Work Week Cost: {formatCurrency(data?.all_costs)}</h2>
             <h3>Activities:</h3>
         </Container>)
 }
