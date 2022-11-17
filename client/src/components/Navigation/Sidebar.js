7e45b904cb3219614fefc8ce8f0c5537b712cffb
Brian <b.perez3237@gmail.com>
2022-11-16 19:28:08 -0500

    Fix sidebar styling

--- client/src/components/Navigation/Sidebar.js
+++ client/src/components/Navigation/Sidebar.js
@@ -17,7 +17,7 @@ function Sidebar() {
   const iconSize = '1.3em'
 
   return (
-    <div className='Sidebar' style={{width:(showSidebar ? '5%' : '30%')}}>
+    <div className='Sidebar' style={{width:(showSidebar ? '8%' : '30%')}}>
             <ul className='Sidebar-list'>
                 <li id='Sidebar-heading' className='row'  onClick={()=>setShowSidebar(!showSidebar)}>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}><h3>Menu</h3></div>
