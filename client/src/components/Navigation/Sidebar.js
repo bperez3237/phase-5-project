be7fdc4b7e04e656183fa694d6cb4f60a5ebe000
Brian <b.perez3237@gmail.com>
2023-02-25 23:57:37 -0500

    Add highlight to selected sidebar page styling

--- client/src/components/Navigation/Sidebar.js
+++ client/src/components/Navigation/Sidebar.js
@@ -11,9 +11,16 @@ import {TbReportMoney} from 'react-icons/tb'
 
 function Sidebar({disable}) {
   const history = useHistory()
-
+  const [selected, setSelected] = useState('home')
   const [showSidebar, setShowSidebar] = useState(false)
   const iconSize = '1.3em'
+
+  const row_class = (name) => {
+    if (name === selected) {
+      return 'row-selected'
+    }
+    return 'row'
+  }
 
   return (
     <div className='Sidebar' style={{'width':(showSidebar ? '50px' : '300px'),'minWidth':(showSidebar ? '50px' : '300px')}}>
@@ -22,23 +29,38 @@ function Sidebar({disable}) {
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}><h3>Menu</h3></div>
                     <div id='icon'>{showSidebar ? <AiOutlineBars size={iconSize} /> : <AiFillCloseSquare size={iconSize} />}</div>
                 </li>
-                <li id='home' className='row'  onClick={()=>history.push('/')}>
+                <li id='home' className={row_class('home')}  onClick={()=>{
+                        history.push('/')
+                        setSelected('home')
+                    }}>
                     <div id='icon'><ImHome size={iconSize} /></div>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>Home</div>
                 </li>
-                <li id='view' className='row'  onClick={()=>history.push('/view')}>
+                <li id='view' className={row_class('view')}   onClick={()=>{
+                        history.push('/view')
+                        setSelected('view')
+                    }}>
                     <div id='icon' ><VscOpenPreview size={iconSize} /></div>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>View Tables</div>
                 </li>
-                <li id='upload' className='row'  onClick={()=>history.push('/upload_review_activities')}>
+                <li id='upload' className={row_class('upload')}   onClick={()=>{
+                        history.push('/upload_review_activities')
+                        setSelected('upload')
+                    }}>
                     <div id='icon' ><MdOutlineConstruction size={iconSize} /></div>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>Upload & Review Activities</div>
                 </li>
-                <li id='enter' className='row'  onClick={()=>history.push('/enter')}>
+                <li id='enter' className={row_class('enter')}  onClick={()=>{
+                        history.push('/enter')
+                        setSelected('enter')
+                    }}>
                     <div id='icon' ><FaPencilRuler size={iconSize} /></div>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>Enter Units</div>
                 </li>
-                <li id='report' className='row'  onClick={()=>history.push('/report')}>
+                <li id='report' className={row_class('report')}   onClick={()=>{
+                        history.push('/report')
+                        setSelected('report')
+                    }}>
                     <div id='icon' ><TbReportMoney size={iconSize} /></div>
                     <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>Weekly Cost Report</div>
                 </li>
