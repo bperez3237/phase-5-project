03ab278297bfbe63c5dc04ef6939a4c3c9b2e37c
Brian <b.perez3237@gmail.com>
2023-02-28 20:51:35 -0500

    Change typescript config

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,28 +1,20 @@
-import {useContext} from 'react'
 import ProjectInfo from "./ProjectInfo";
-import { WorkWeekContext} from '../../context/WorkWeekContext'
 import WeekSelect from "./WeekSelect";
 import PagesInfo from './PagesInfo';
 import WeekStatus from './WeekStatus';
 import './Home.css'
 
 function Home() {
-    const {workWeek} = useContext(WorkWeekContext)
 
     return(
         <div className='page-container homepage' >
-            {/* <div className='element heading'>
-                <h2>Home Week of {workWeek!==[] ? workWeek.end_date : ' - not yet selected -'}</h2>   
-            </div> 
-            <WeekSelect />
-            <ProjectInfo />
-            <PagesInfo /> */}
             <div className='home-header'>
                 <WeekSelect />
                 <WeekStatus />
             </div>
-            <ProjectInfo />
+            
             <PagesInfo />
+            <ProjectInfo />
         </div>
         
     )
